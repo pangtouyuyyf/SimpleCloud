0c2a1b0f1fd491f0e1d1ca0159bc451a18de8395
pangtouyuyyf <chendreaming@126.com>
2020-03-27 16:58:21 +0800

    celebration day

---
+++ SimpleGateway/src/main/java/com/simple/manage/SimpleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.simple.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGatewayApplication.class, args);
+    }
+
+}
