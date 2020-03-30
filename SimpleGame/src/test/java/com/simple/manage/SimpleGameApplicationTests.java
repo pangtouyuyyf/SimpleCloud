e6bae18b273c68f560c2e8faaf8965d9bb8f56da
pangtouyuyyf <chendreaming@126.com>
2020-03-30 16:38:24 +0800

    celebration day

---
+++ SimpleGame/src/test/java/com/simple/manage/SimpleGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
