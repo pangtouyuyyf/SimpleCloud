ee75bcce234837dc5817dd76823b39024c23671b
pangtouyuyyf <chendreaming@126.com>
2020-03-31 16:47:40 +0800

    celebration day

--- SimpleWeb/src/main/java/com/simple/manage/SimpleWebApplication.java
+++ SimpleWeb/src/main/java/com/simple/manage/SimpleWebApplication.java
@@ -2,8 +2,10 @@ package com.simple.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SimpleWebApplication {
 
     public static void main(String[] args) {
