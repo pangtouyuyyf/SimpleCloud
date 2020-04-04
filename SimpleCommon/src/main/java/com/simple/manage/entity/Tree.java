4163f806837a29b04bf7ef1ba67d36ce215c477e
pangtouyuyyf <chendreaming@126.com>
2020-04-05 00:21:06 +0800

    celebration day

--- SimpleCommon/src/main/java/com/simple/manage/entity/Tree.java
+++ SimpleCommon/src/main/java/com/simple/manage/entity/Tree.java
@@ -1,10 +1,21 @@
 package com.simple.manage.entity;
 
+import lombok.Data;
+
+import java.util.List;
+
 /**
  * Description 树
  * Author chen
  * CreateTime 2020-04-04 23:44
  **/
-
+@Data
 public class Tree {
+    private int key;
+
+    private String title;
+
+    private boolean isLeaf;
+
+    private List<Tree> children;
 }
