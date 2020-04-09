a5fc65657746db2edb1b92639b0d92fb90ed7334
pangtouyuyyf <chendreaming@126.com>
2020-04-09 22:29:07 +0800

    celebration day

--- SimpleWeb/src/main/java/com/simple/manage/client/auth/fallback/LoginClientFallback.java
+++ SimpleWeb/src/main/java/com/simple/manage/client/auth/fallback/LoginClientFallback.java
@@ -1,6 +1,10 @@
 package com.simple.manage.client.auth.fallback;
 
 import com.simple.manage.client.auth.LoginClient;
+import com.simple.manage.domain.Result;
+import com.simple.manage.enums.SysExpEnum;
+import com.simple.manage.util.LogUtil;
+import com.simple.manage.util.ResultUtil;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,4 +14,11 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class LoginClientFallback implements LoginClient {
+    private static final String SERVICE_NAME_MSG = "simple-auth服务";
+
+    public Result<?> login(String name, String pwd,
+                           String channel) {
+        LogUtil.error(LoginClientFallback.class, SERVICE_NAME_MSG + "sys/login接口连接异常!");
+        return ResultUtil.error(SysExpEnum.COMMON_ERROR);
+    }
 }
