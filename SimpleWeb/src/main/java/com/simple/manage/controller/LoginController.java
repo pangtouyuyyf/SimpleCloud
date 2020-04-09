4f17dfac41446572a81a06d640b1e31c7e6804d7
pangtouyuyyf <chendreaming@126.com>
2020-04-09 22:52:55 +0800

    celebration day

---
+++ SimpleWeb/src/main/java/com/simple/manage/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.simple.manage.controller;
+
+import com.simple.manage.client.auth.LoginClient;
+import com.simple.manage.domain.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Description 登录controller
+ * Author chen
+ * CreateTime 2020-04-08 17:11
+ **/
+@RestController
+@RequestMapping(value = "/sys")
+public class LoginController extends WebController {
+    @Resource
+    private LoginClient loginClient;
+
+    @GetMapping("/login")
+    public Result<?> login(@RequestParam("name") String loginName,
+                           @RequestParam("pwd") String password,
+                           @RequestParam("channel") String channel) throws Exception{
+        return loginClient.login(loginName, password, channel);
+    }
+}
