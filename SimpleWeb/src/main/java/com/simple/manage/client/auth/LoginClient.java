cf6acd913ac39bab2388b459e1e5a01fd1bd2bb8
pangtouyuyyf <chendreaming@126.com>
2020-04-09 22:29:15 +0800

    celebration day

--- SimpleWeb/src/main/java/com/simple/manage/client/auth/LoginClient.java
+++ SimpleWeb/src/main/java/com/simple/manage/client/auth/LoginClient.java
@@ -1,13 +1,19 @@
 package com.simple.manage.client.auth;
 
 import com.simple.manage.client.auth.fallback.LoginClientFallback;
+import com.simple.manage.domain.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Description 权限服务请求客户端
  * Author chen
  * CreateTime 2020-04-01 15:37
  **/
-@FeignClient(name = "simple-gate", contextId = "AuthClient", fallback = LoginClientFallback.class)
+@FeignClient(name = "simple-gate", contextId = "AuthSysClient", fallback = LoginClientFallback.class)
 public interface LoginClient {
+    @GetMapping("simple-auth/sys/login")
+    Result<?> login(@RequestParam("name") String name, @RequestParam("pwd") String pwd,
+                    @RequestParam("channel") String channel);
 }
