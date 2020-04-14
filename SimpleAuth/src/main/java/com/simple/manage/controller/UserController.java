f0dbdd3a9279a42353b9765646c3559a9008a40b
pangtouyuyyf <chendreaming@126.com>
2020-04-14 22:34:38 +0800

    celebration day

---
+++ SimpleAuth/src/main/java/com/simple/manage/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.simple.manage.controller;
+
+import com.simple.manage.domain.LoginInfo;
+import com.simple.manage.domain.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description 用户信息操作controller
+ * Author chen
+ * CreateTime 2020-04-14 22:03
+ **/
+@RestController
+@RequestMapping(value = "/user")
+public class UserController extends BaseController {
+    /**
+     * 检查当前登录用户是否为系统中用户
+     *
+     * @return
+     */
+    @GetMapping("/checkOne")
+    public Result<?> checkOne() {
+        LoginInfo info = this.getLoginInfo();
+        info.getCurrId();
+        return success();
+    }
+}
