c34acd00d8d78a96bb107edf19d10c741ebe8cbd
pangtouyuyyf <chendreaming@126.com>
2020-04-19 00:04:49 +0800

    celebration day

---
+++ SimpleBase/src/main/java/com/simple/manage/config/RedisConfig.java
@@ -0,0 +1,62 @@
+package com.simple.manage.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Description redis 配置
+ * Author chen
+ * CreateTime 2020-04-18 23:49
+ **/
+
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory).build();
+        return redisCacheManager;
+    }
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        // 配置连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // key序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        //json 转对象类，不设置默认的会将json转成hashmap
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会抛出异常
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 值采用json序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+        // 设置hash key 和value序列化模式
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ SimpleBase/src/main/java/com/simple/manage/controller/RedisController.java
@@ -0,0 +1,12 @@
+package com.simple.manage.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description redis 服务controller
+ * Author chen
+ * CreateTime 2020-04-18 11:51
+ **/
+@RestController
+public class RedisController {
+}
