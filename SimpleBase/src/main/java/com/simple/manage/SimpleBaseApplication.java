90f1ece5d8a552df7cb30e0f77057baae00bb55c
pangtouyuyyf <chendreaming@126.com>
2020-04-18 11:47:10 +0800

    celebration day

---
+++ SimpleBase/src/main/java/com/simple/manage/SimpleBaseApplication.java
@@ -0,0 +1,13 @@
+package com.simple.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBaseApplication.class, args);
+    }
+
+}
