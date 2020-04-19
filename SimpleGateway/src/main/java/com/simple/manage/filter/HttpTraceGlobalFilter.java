c6f0baf143d6271068d9f3c3312126d0364ab721
pangtouyuyyf <chendreaming@126.com>
2020-04-19 22:26:27 +0800

    celebration day

--- SimpleGateway/src/main/java/com/simple/manage/filter/HttpTraceGlobalFilter.java
+++ SimpleGateway/src/main/java/com/simple/manage/filter/HttpTraceGlobalFilter.java
@@ -2,14 +2,15 @@ package com.simple.manage.filter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simple.manage.client.BaseClient;
+import com.simple.manage.config.JwtConfig;
 import com.simple.manage.domain.Result;
+import com.simple.manage.domain.Token;
 import com.simple.manage.enums.SysExpEnum;
 import com.simple.manage.util.CommonUtil;
 import com.simple.manage.util.JwtUtil;
 import com.simple.manage.util.LogUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -22,6 +23,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.Resource;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -35,23 +37,23 @@ import java.util.Map;
  **/
 @Component
 public class HttpTraceGlobalFilter implements GlobalFilter, Ordered {
-    private Logger logger = LoggerFactory.getLogger(HttpTraceGlobalFilter.class);
+    @Resource
+    private BaseClient baseClient;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
-        DataBuffer dataBuffer = null;
 
         String token = request.getHeaders().getFirst(CommonUtil.TOKEN);
         Map<String, String> jwtMap = JwtUtil.parseJWT(token);
 
+        Result r = null;
+
         /** 验证令牌合法性 **/
         if (jwtMap == null) {
             LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌验证失败");
-            response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-            dataBuffer = response.bufferFactory().wrap(getErrorResult(Result.error(SysExpEnum.NEED_LOGIN)).getBytes());
-            return response.writeWith(Mono.just(dataBuffer));
+            return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.NEED_LOGIN)));
         }
 
         /** 获取令牌中的用户、角色和登录渠道 **/
@@ -63,16 +65,57 @@ public class HttpTraceGlobalFilter implements GlobalFilter, Ordered {
                 || StringUtils.isNoneEmpty(channel)
                 || !(CommonUtil.CHANNEL_WEB.equals(channel) || CommonUtil.CHANNEL_APP.equals(channel))) {
             LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌参数有误");
-            response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-            dataBuffer = response.bufferFactory().wrap(getErrorResult(Result.error(SysExpEnum.NEED_LOGIN)).getBytes());
-            return response.writeWith(Mono.just(dataBuffer));
+            return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.NEED_LOGIN)));
+
         }
 
         /** 获取服务器缓存令牌 **/
         List<String> tokenKeyParts = Arrays.asList(CommonUtil.TOKEN_PREFIX, userId, channel);
         String tokenRedisKey = String.join(CommonUtil.UNDERLINE, tokenKeyParts);
-//        String tokenRedis = this.redisOperation.getStr(tokenRedisKey);
+        r = baseClient.getToken(tokenRedisKey);
+        if (!r.isSuccess()) {
+            LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌查询失败");
+            return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.CONNECT_OR_OVERTIME_ERROR)));
+        }
+        Token tokenRedis = (Token) r.getData();
 
+        /** 验证令牌缓存情况 **/
+        if (StringUtils.isNoneEmpty(tokenRedis.getValue())) {
+            LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌缓存缺失");
+            return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.NEED_LOGIN)));
+        }
+
+        /** 查看剩余有效时间 **/
+        long time = tokenRedis.getTime();
+        if (time < 1) {
+            LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌缓存失效");
+            return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.LOGIN_AGAIN)));
+        }
+
+        /** 比对redis内令牌和传入令牌是否一致，防止劫持前一次有效令牌做操作 **/
+        if (JwtConfig.ANTI_HIJACK) {
+            if (tokenRedis.getValue().compareTo(token) != 0) {
+                LogUtil.error(HttpTraceGlobalFilter.class, LocalDateTime.now() + " 令牌比对失败");
+                return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.REMOTE_LOGIN)));
+            }
+        }
+
+        /** 令牌续权 **/
+        if (JwtConfig.ENABLE_RENEW) {
+            if (CommonUtil.CHANNEL_WEB.equals(channel)) {
+                if (time < JwtConfig.WEB_UPDATE_INTERVAL) {
+                    r = baseClient.renewToken(tokenRedisKey, JwtConfig.WEB_LIFE_CYCLE);
+
+                }
+            } else {
+                if (time < JwtConfig.APP_UPDATE_INTERVAL) {
+                    r = baseClient.renewToken(tokenRedisKey, JwtConfig.APP_LIFE_CYCLE);
+                }
+            }
+            if (!r.isSuccess()) {
+                return response.writeWith(Mono.just(handleResponse(response, SysExpEnum.CONNECT_OR_OVERTIME_ERROR)));
+            }
+        }
 
         URI originalRequestUrl = request.getURI();
         System.out.print("from:");
@@ -86,6 +129,19 @@ public class HttpTraceGlobalFilter implements GlobalFilter, Ordered {
     public int getOrder() {
         return -10;
     }
+
+    /**
+     * 异常response返回处理
+     *
+     * @param response
+     * @param expEnum
+     * @return
+     */
+    private DataBuffer handleResponse(ServerHttpResponse response, SysExpEnum expEnum) {
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        return response.bufferFactory().wrap(getErrorResult(Result.error(expEnum)).getBytes());
+    }
+
 
     /**
      * 获取异常结果
