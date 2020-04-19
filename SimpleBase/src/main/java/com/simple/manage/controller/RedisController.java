e0a3893c77b1fdf6f77f5764002a1eab2144031d
pangtouyuyyf <chendreaming@126.com>
2020-04-19 11:34:39 +0800

    celebration day

--- SimpleBase/src/main/java/com/simple/manage/controller/RedisController.java
+++ SimpleBase/src/main/java/com/simple/manage/controller/RedisController.java
@@ -1,6 +1,13 @@
 package com.simple.manage.controller;
 
+import com.simple.manage.component.RedisOperation;
+import com.simple.manage.domain.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Description redis 服务controller
@@ -8,5 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  * CreateTime 2020-04-18 11:51
  **/
 @RestController
+@RequestMapping(value = "/redis")
 public class RedisController {
+    @Resource
+    private RedisOperation redisOperation;
+
+    @GetMapping("/getToken")
+    public Result<?> getToken(@RequestParam("key") String key) {
+        String token = redisOperation.getStr(key);
+        return Result.success(token);
+    }
+
+    @GetMapping("/setStr")
+    public Result<?> setStr(@RequestParam("key") String key, @RequestParam("value") String value, @RequestParam("time") Long time) {
+        redisOperation.setStr(key, value, time);
+        return Result.success();
+    }
 }
