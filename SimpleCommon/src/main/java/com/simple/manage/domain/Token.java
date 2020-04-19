6b641a194a72fd4802eab89f12bfb25313155741
pangtouyuyyf <chendreaming@126.com>
2020-04-19 11:52:34 +0800

    celebration day

---
+++ SimpleCommon/src/main/java/com/simple/manage/domain/Token.java
@@ -0,0 +1,17 @@
+package com.simple.manage.domain;
+
+import lombok.Data;
+
+/**
+ * Description token
+ * Author chen
+ * CreateTime 2020-04-19 11:51
+ **/
+@Data
+public class Token {
+    private String key;
+
+    private String value;
+
+    private long time;  //剩余有效时间
+}
