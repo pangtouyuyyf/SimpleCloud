167c174a9a5b6ec2db6d163348f1440da7203619
pangtouyuyyf <chendreaming@126.com>
2020-04-19 22:31:14 +0800

    celebration day

--- SimpleGateway/src/main/java/com/simple/manage/client/BaseClient.java
+++ SimpleGateway/src/main/java/com/simple/manage/client/BaseClient.java
@@ -1,6 +1,6 @@
 package com.simple.manage.client;
 
-import com.simple.manage.client.fallback.AuthClientFallback;
+import com.simple.manage.client.fallback.BaseClientFallback;
 import com.simple.manage.domain.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Author chen
  * CreateTime 2020-04-19 11:35
  **/
-@FeignClient(name = "simple-base", contextId = "BaseClient", fallback = AuthClientFallback.class)
+@FeignClient(name = "simple-base", contextId = "BaseClient", fallback = BaseClientFallback.class)
 public interface BaseClient {
     @GetMapping("redis/getToken")
     Result<?> getToken(@RequestParam("key") String key);
