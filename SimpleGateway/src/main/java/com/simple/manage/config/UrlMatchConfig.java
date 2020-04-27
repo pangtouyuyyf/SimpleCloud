1fa26b457b1f10073cff7adddc1d221d35d3de51
pangtouyuyyf <chendreaming@126.com>
2020-04-27 10:24:45 +0800

    celebration day

--- SimpleGateway/src/main/java/com/simple/manage/config/UrlMatchConfig.java
+++ SimpleGateway/src/main/java/com/simple/manage/config/UrlMatchConfig.java
@@ -15,17 +15,10 @@ public class UrlMatchConfig {
     /* 无需token验证的url */
     private static List<String> URL_ABS_PERMIT = Arrays.asList("/simple-auth/sys/login", "");
 
-    /* 前端禁止访问的url */
-    private static List<String> URL_FORBID = Arrays.asList("/simple-base/redis/**", "");
-
     /* ant匹配 */
     private static final AntPathMatcher ANT_PATH_MATCHER = new AntPathMatcher();
 
     public static boolean isIgnoreToken(String path) {
         return URL_ABS_PERMIT.stream().anyMatch((url) -> path.startsWith(url) || ANT_PATH_MATCHER.match(url, path));
-    }
-
-    public static boolean isForbidden(String path) {
-        return URL_FORBID.stream().anyMatch((url) -> path.startsWith(url) || ANT_PATH_MATCHER.match(url, path));
     }
 }
