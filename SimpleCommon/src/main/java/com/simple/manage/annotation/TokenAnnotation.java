30b30b6a3c819bf7d902a45c6171342117788b00
pangtouyuyyf <chendreaming@126.com>
2020-04-27 20:34:34 +0800

    celebration day

---
+++ SimpleCommon/src/main/java/com/simple/manage/annotation/TokenAnnotation.java
@@ -0,0 +1,14 @@
+package com.simple.manage.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Description 令牌校验自定义注解
+ * Author chen
+ * CreateTime 2020-04-27 20:33
+ **/
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TokenAnnotation {
+}
