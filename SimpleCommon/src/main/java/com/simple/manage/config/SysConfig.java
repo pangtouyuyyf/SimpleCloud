44babf53bbf286f79c24dbac9e4c63587f24b902
pangtouyuyyf <chendreaming@126.com>
2020-05-01 13:13:17 +0800

    celebration day

---
+++ SimpleCommon/src/main/java/com/simple/manage/config/SysConfig.java
@@ -0,0 +1,17 @@
+package com.simple.manage.config;
+
+/**
+ * Description 系统配置
+ * Author chen
+ * CreateTime 2020-05-01 13:10
+ **/
+
+public class SysConfig {
+    public static final boolean ENABLE_AUZ = true;  //是否开启权限验证
+
+    private static final boolean SHOW_IP = true;  //是否显示IP
+
+    private static final String DEFAULT_PWD = "123456";  //系统重置登录密码
+
+    private static final boolean IS_CLEAN_LOGIN_INFO = true;  //退出登录是否清除当前登录信息缓存
+}
