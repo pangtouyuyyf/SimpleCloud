5eac4d644fa6eb193a1e97dc63689df6d10a5ca9
pangtouyuyyf <chendreaming@126.com>
2020-05-01 23:59:44 +0800

    celebration day

--- SimpleCommon/src/main/java/com/simple/manage/domain/Token.java
+++ SimpleCommon/src/main/java/com/simple/manage/domain/Token.java
@@ -1,6 +1,8 @@
 package com.simple.manage.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Description token
@@ -8,19 +10,12 @@ import lombok.Data;
  * CreateTime 2020-04-19 11:51
  **/
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Token {
     private String key;
 
     private String value;
 
     private long time;  //剩余有效时间
-
-    public Token() {
-    }
-
-    public Token(String key, String value, long time) {
-        this.key = key;
-        this.value = value;
-        this.time = time;
-    }
 }
