bf61f6d31e1beaac8ab650bf0f041a6b9b0e4e39
pangtouyuyyf <chendreaming@126.com>
2020-05-04 10:42:39 +0800

    celebration day

---
+++ SimpleAuto/src/test/java/com/simple/manage/SimpleAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
