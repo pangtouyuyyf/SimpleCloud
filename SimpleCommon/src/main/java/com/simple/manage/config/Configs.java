5c1b037fe1495839358666d91b3d8d9803665515
chen <chendreaming@126.com>
2020-05-15 13:36:15 +0800

    celebration day

--- SimpleCommon/src/main/java/com/simple/manage/config/Configs.java
+++ SimpleCommon/src/main/java/com/simple/manage/config/Configs.java
@@ -100,4 +100,36 @@ public class Configs {
         /* 退出登录是否清除当前登录信息缓存 */
         public static final boolean IS_CLEAN_LOGIN_INFO = true;
     }
+
+    /**
+     * jwt配置
+     */
+    public class Jwt {
+        /* jwt发行者 */
+        public static final String ISSUER = "chendreaming@126.com";
+
+        /* jwt基础64位秘钥 */
+        public static final String BASE_64_SECRET = "VHVubmVsJTIwT2YlMjBMb3Zl";
+
+        /* jwt web端作用时间:秒(这里为10天) */
+        public static final int WEB_LIFE_CYCLE = 864000;
+
+        /* jwt web更新间隔:秒(这里为1天) */
+        public static final int WEB_UPDATE_INTERVAL = 86400;
+
+        /* jwt app端作用时间:秒(这里为10天) */
+        public static final int APP_LIFE_CYCLE = 864000;
+
+        /* jwt app更新间隔:秒(这里为1天) */
+        public static final int APP_UPDATE_INTERVAL = 86400;
+
+        /* login info更新间隔:秒(这里为10天) */
+        public static final int LOGIN_INFO_LIFE_CYCLE = 86400;
+
+        /* 开启自动令牌续权 */
+        public static final boolean ENABLE_RENEW = true;
+
+        /* 开启令牌反劫持 */
+        public static final boolean ANTI_HIJACK = true;
+    }
 }
