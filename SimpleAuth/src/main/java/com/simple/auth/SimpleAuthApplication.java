51cf40ce1fa21eda2a02e6c03ede96094920b92b
chen <chendreaming@126.com>
2020-06-05 16:32:40 +0800

    celebration day

--- SimpleAuth/src/main/java/com/simple/auth/SimpleAuthApplication.java
+++ SimpleAuth/src/main/java/com/simple/auth/SimpleAuthApplication.java
@@ -1,11 +1,14 @@
 package com.simple.auth;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.simple")
+@MapperScan(basePackages = "com.simple")
 public class SimpleAuthApplication {
 
     public static void main(String[] args) {
