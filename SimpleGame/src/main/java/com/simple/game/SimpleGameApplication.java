e47d37fb83272bfd8c1139fdc0eb97199d04f6d6
pangtouyuyyf <chendreaming@126.com>
2020-12-25 16:08:13 +0800

    celebration day

--- SimpleGame/src/main/java/com/simple/game/SimpleGameApplication.java
+++ SimpleGame/src/main/java/com/simple/game/SimpleGameApplication.java
@@ -1,9 +1,13 @@
 package com.simple.game;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
+@MapperScan(basePackages = "com.simple.access")
 public class SimpleGameApplication {
 
     public static void main(String[] args) {
