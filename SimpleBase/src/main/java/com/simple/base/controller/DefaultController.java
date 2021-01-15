d0c3bd268d82014889585bd90fe56e1db376ec7f
pangtouyuyyf <chendreaming@126.com>
2021-01-15 11:44:24 +0800

    celebration day

---
+++ SimpleBase/src/main/java/com/simple/base/controller/DefaultController.java
@@ -0,0 +1,26 @@
+package com.simple.base.controller;
+
+import com.simple.common.domain.Result;
+import com.simple.common.enums.SysExpEnum;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description 默认服务controller
+ * Author chen
+ * CreateTime 2020-04-18 11:51
+ **/
+@RestController
+@RequestMapping(value = "/default")
+public class DefaultController {
+    /**
+     * 服务熔断默认错误返回
+     *
+     * @return
+     */
+    @GetMapping("/fail")
+    public Result<?> getToken() {
+        return Result.message(SysExpEnum.CONNECT_OR_OVERTIME_ERROR.getCode(), SysExpEnum.CONNECT_OR_OVERTIME_ERROR.getMessage(), null, null);
+    }
+}
