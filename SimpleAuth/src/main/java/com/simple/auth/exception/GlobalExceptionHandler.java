426f4db36ceca57ac9c76958325d56df957ff132
pangtouyuyyf <chendreaming@126.com>
2021-01-20 10:30:58 +0800

    celebration day

--- SimpleAuth/src/main/java/com/simple/auth/exception/GlobalExceptionHandler.java
+++ SimpleAuth/src/main/java/com/simple/auth/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import com.simple.common.domain.Result;
 import com.simple.common.enums.SysExpEnum;
 import com.simple.common.util.LogUtil;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * Description controller全局异常处理
@@ -25,13 +27,16 @@ public class GlobalExceptionHandler {
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public Result<?> commonHandler(Exception ex) {
-        LogUtil.error(GlobalExceptionHandler.class, ExceptionUtils.getStackTrace(ex));
-
         // 判断异常类型
-//        if (e instanceof MissingServletRequestParameterException) {
-//
-//
-//        }
+        if (ex instanceof MissingServletRequestParameterException) {
+            //访问参数异常
+            LogUtil.error(GlobalExceptionHandler.class, "访问参数异常:{}", ExceptionUtils.getStackTrace(ex));
+        } else if (ex instanceof NoHandlerFoundException) {
+            //404-没有可访问的资源
+            LogUtil.error(GlobalExceptionHandler.class, "没有可访问的资源:{}", ExceptionUtils.getStackTrace(ex));
+        } else {
+            LogUtil.error(GlobalExceptionHandler.class, ExceptionUtils.getStackTrace(ex));
+        }
 
         return Result.error(SysExpEnum.COMMON_ERROR);
     }
